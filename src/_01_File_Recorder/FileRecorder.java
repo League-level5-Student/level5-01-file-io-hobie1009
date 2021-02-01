2b504d2508b108c6851308ce5dd5cf3b157a6b77
hobsterthemeep <hobsterthemeep@localhost>
2021-01-31 17:57:50 -0800

    Finished module 0 and started on module 1, files look complicated

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,22 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		String value = JOptionPane.showInputDialog("write a message here");
+		try {
+			FileWriter fw = new FileWriter ("src/_01_File_Recorder/Recorder.txt");
+			fw.write(value);
+			fw.close();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
 }
